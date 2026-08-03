89d0f7e27c727d063b04609c5094abcbf943b50c
nishant00155 <nishantkrishna930@gmail.com>
2026-08-03 19:58:10 +0530

    update the cors so can access new frontend route

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ app.use(cookieParser())
 
 app.use(express.json())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"arambh.site",
     credentials:true
 }))
 /* require all the routes here */
