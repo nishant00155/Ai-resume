047ac20302e803c3381e50aab218b138c1f38068
nishant00155 <nishantkrishna930@gmail.com>
2026-08-03 16:53:13 +0530

    add dependencie jsonwebtoken with frontend conected to new backend route

--- frontend/src/features/interview/services/interview.api.js
+++ frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/interview",
+  baseURL: "https://ai-resume-44y9.onrender.com/api/interview",
   withCredentials: true,
 });
 
