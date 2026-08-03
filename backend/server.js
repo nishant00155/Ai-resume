f4928b722ecb4c568962e50976d550f0564a3e3a
nishant00155 <nishantkrishna930@gmail.com>
2026-08-04 00:48:38 +0530

    env error while deploying

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, ".env") });
+
 const app = require("./src/app");
 const connectToDb = require("./src/config/database");
 
-
-
-connectToDb()
-
+connectToDb();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
+  console.log(`server is running on port ${PORT}`);
 });
